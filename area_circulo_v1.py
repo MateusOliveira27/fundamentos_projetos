4a6dbe3b0905118722e1f2200d4da4379cbd1e20
MateusOlivera27 <mateusoliveira13k@gmail.com>
2020-09-09 23:30:25 -0500

    area_circulo_v6

--- area_circulo_v1.py
+++ area_circulo_v1.py
@@ -1,6 +1,11 @@
 #!/usr/local/bin/python3
 from math import pi
 
+
+def circulo(raio):
+    print('Área do círculo', pi * float(raio) ** 2)
+    
+
 if __name__ == '__main__':
     raio = input('informe o raio: ')
-    print('Área do círculo', pi * float(raio) ** 2)
+    circulo(raio)
