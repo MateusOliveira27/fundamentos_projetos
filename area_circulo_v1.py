70335249fdbf65c4bb700120e576232472425f2b
MateusOlivera27 <mateusoliveira13k@gmail.com>
2020-09-12 11:04:07 -0500

    area_circulo_v13

--- area_circulo_v1.py
+++ area_circulo_v1.py
@@ -4,8 +4,13 @@ import sys
 import errno
 
 
+class TerminalColor:
+    ERRO ='\033[91m'
+    NORMAL ='\033[0m'
+
+
 def help():
-    print("É necessário informar o raio do círculo.")    
+    print("É necessário informar o raio do círculo.")
     print("Sintaxe: {} <raio>".format(sys.argv[0][2:]))
 
 
@@ -15,14 +20,15 @@ def circulo(raio):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-       help()  
-       sys.exit(errno.EPERM)
+        help()
+        sys.exit(errno.EPERM)
 
     if not sys.argv[1].isnumeric():
         help()
-        print('O raio deve ser um valor númerico')
-        sys.exit(errno.EINVAL)  
-    
+        print(TerminalColor.ERRO +'O raio deve ser um valor númerico' +
+              TerminalColor.NORMAL)
+        sys.exit(errno.EINVAL)
+
     raio = sys.argv[1]
     area = circulo(raio)
     print('Área do círculo :', area)
